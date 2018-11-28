88e987778ba48b9b6998bda2ccbee1278e242203
lingyunyi <43637894+lingyunyi@users.noreply.github.com>
2018-11-28 11:52:00 +0800

    Add files via upload

--- ExcelInsert/ExcelInsert.py
+++ ExcelInsert/ExcelInsert.py
@@ -134,18 +134,22 @@ def readFile_returnPath(pathFile,pathList):
     allFiles = os.listdir(pathFile)
     # 遍历所有文件
     for i in range(len(allFiles)):
-        # 获得所有文件的单个文件，并且合并路径
-        path = os.path.join(pathFile,allFiles[i])
-        # 如果是文件
-        if os.path.isfile(path):
-            # 判断是否是Excel文件
-            if path.split(".")[-1] == "xls" or path.split(".")[-1] == "xlsx":
-                # 加入文件列表
-                pathList.append(path)
-        # 如果是目录
-        if os.path.isdir(path):
-            # 再次遍历传入的文件夹
-            readFile_returnPath(path,pathList)
+        try:
+            # 获得所有文件的单个文件，并且合并路径
+            path = os.path.join(pathFile,allFiles[i])
+            # 如果是文件
+            if os.path.isfile(path):
+                # 判断是否是Excel文件
+                if path.split(".")[-1] == "xls" or path.split(".")[-1] == "xlsx":
+                    # 加入文件列表
+                    pathList.append(path)
+            # 如果是目录
+            if os.path.isdir(path):
+                # 再次遍历传入的文件夹
+                readFile_returnPath(path,pathList)
+        except BaseException as tip:
+            print("BedFalse：",tip)
+            continue
     return pathList
 def mainFunction(pathFile):
     for path in pathList:
