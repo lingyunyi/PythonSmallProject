594211ea9336c7b8324f069bacfd7df4ba674d3e
lingyunyi <43637894+lingyunyi@users.noreply.github.com>
2019-10-13 20:32:10 +0800

    1、新添新的旅游网页的爬虫抓取
    1、新添新的旅游网页的爬虫抓取
    2、新的旅游网页抓取任务未完成，剩余数据处理，以及IMG地址未抓取的问题

--- TouristCrawler/TouristCrawler.py
+++ TouristCrawler/TouristCrawler.py
@@ -1,5 +1,8 @@
 import requests
 import re
+from bs4 import BeautifulSoup
+
+
 
 class TouristCrawler(object):
 
@@ -71,6 +74,95 @@ class TouristCrawler(object):
             print("Error:", error)
 
 
+    def MaFengWo(self):
+        try:
+            weburl = "http://www.mafengwo.cn/mdd/base/routeline/pagedata_routelist"
+            header = {
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36",
+            }
+            mdd_src_list = []
+            for i in range(1, 3):
+                data = {
+                    "mddid":"12810",
+                    "page":"%s"%(i),
+                    "type":"2",
+                    "_ts":"1570885642489",
+                    "_sn":"b3f692eb14",
+
+                }
+                r = requests.post(weburl, headers=header, verify=False, allow_redirects=True, timeout=5, data=data)
+                ALLHTML = r.text.replace('\\',"")
+                ALLHTML2 = ALLHTML.encode("utf-8").decode('unicode_escape')
+                DIV_A = re.findall('<a href="(.*?)" target="_blank">',ALLHTML2)
+                for i in DIV_A:
+                    if "/mdd/route" in i:
+                        i = 'http://www.mafengwo.cn' + i
+                        mdd_src_list.append(i)
+
+            mdd_src_set = set(mdd_src_list)
+            print(mdd_src_set)
+            self.MafengWo2(mdd_src_set)
+        except BaseException as error:
+            print("Error:", error)
+
+    def MafengWo2(self,urlSet):
+        try:
+            # 循环传入过来的所有连接
+            for i in urlSet:
+                print("BeginStatr:%s" %(i))
+                # i = "http://www.mafengwo.cn/mdd/route/12810_101612.html"
+                header = {
+                    "referer":"%s" %(i),
+                    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36"
+                }
+                r = requests.get(i, headers=header, verify=False, allow_redirects=True, timeout=5)
+                # 替换掉被魔改的内容
+                ALLHTML = r.content.decode("utf-8").replace('\\',"")
+                # 获取该路径的标题
+
+                title = re.findall("<h1>(.*?)</h1>",ALLHTML)[0]
+                # 获取该路径，所需要的天数
+                titleDay = re.findall("\d",title)[0] + "天"
+                # 将获得的html文档进行一次文档整合
+                ALLHTML = str(ALLHTML).encode("utf-8").decode("utf-8")
+                # 使用BS4开始处理一些困难的html元素
+                soup = BeautifulSoup(ALLHTML, 'html.parser')
+                # 找到，旅游路径的简单基本时间路线的TAG标签
+                dayWayAll = soup.select(".J_overview .day")
+                if dayWayAll == []:
+                    # 特殊情况，如果该页面没有含有day Class类。
+                    dayWayAll = soup.select(".J_overview a")
+                    dayWay_Time = "第1天"
+                dayWayContent = []
+                # 遍历该便签，将所有获得的内容存入列表中
+                for i in enumerate(dayWayAll):
+                    if dayWay_Time != "":
+                        dayWay_Time = "第" + str(i[0] + 1) + "天"
+                    dayWay_Way = re.findall(">(.*?)</a>",str(i[1]))
+                    dayWay_Way.insert(0,str(dayWay_Time))
+                    dayWayContent.append(dayWay_Way)
+                # 查找下一个具体内容的旅游路程列表
+                wayAllContent = soup.select(".day-item")
+                for i in enumerate(wayAllContent):
+                    # 将路径的评论内容添加到一个内容表格中
+                    dayWay_Way_Content = re.findall('<div class="poi-txt">(.*?)</div>', str(i[1]))
+                    dayWayContent[i[0]].append(str(dayWay_Way_Content))
+                # 再把之前的两个内容，添加到内容列表中
+                dayWayContent.insert(0,titleDay)
+                dayWayContent.insert(0,title)
+
+                for i in dayWayContent:
+                    if isinstance(i,list) == True and len(i) == 2:
+                        dayWayContent.pop()
+
+
+
+                print(dayWayContent)
+
+
+        except BaseException as error:
+            print("2Error:", error)
+
 
 if __name__ == "__main__":
 
@@ -78,4 +170,5 @@ if __name__ == "__main__":
     # for i in range(1,2):
     #     weburl = "https://vacations.ctrip.com/list/grouptravel/d-guangxi-100052.html?p=%s" %(i)
     #     get.XieCheng(weburl)
-    get.QiongYou("")
+    # get.QiongYou("")
+    get.MaFengWo()
