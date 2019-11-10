0b08f920bb4aaeddb0b49da03adc273a20069c3d
lingyunyi <43637894+lingyunyi@users.noreply.github.com>
2019-11-10 19:36:12 +0800

    1、增加获取大部分Biblibili图片。2、优化大量代码，减少代码冗余性。
    1、增加获取大部分Biblibili图片。
    2、优化大量代码，减少代码冗余性。

--- SpaceStation2/WebShell.py
+++ SpaceStation2/WebShell.py
@@ -85,41 +85,36 @@ class webShell(object):
         except BaseException as error:
             print("Web_PING222----------error:\n",error)
 
-    def Get_BiliBili_Img_Url(self,golbalData):
-        try:
-            r = requests.get("https://www.bilibili.com/anime/", headers=self.get_request_headers(), timeout=5)
-            # 获取网页所有HTML标签
-            response = r.content.decode("utf-8")
-            # 获取包含需要结果页面的DIV标签
-            response_DIV = re.findall('<div class="carou-images">(.*?)</div>', response)
-            # 获取所需要的所有A标签
-            response_A = re.findall('a href="(.*?)"', str(response_DIV))
-            # 获取所有需要的DIV标签
-            response_IMG = re.findall('img src="(.*?)@', str(response_DIV))
-            for i in range(len(response_A)):
-                print("获得URL：%s" % (response_A[i]))
-                golbalData["BiliBili"][response_A[i]] = response_IMG[i]
-        except BaseException as error:
-            print("Get_BiliBili_Img_Url----------\n", error)
 
-    def Get_BiliBili_Img_Url_guochan(self,golbalData):
-        try:
-            r = requests.get("https://www.bilibili.com/guochuang", headers=self.get_request_headers(), timeout=5)
-            # 获取网页所有HTML标签
-            response = r.content.decode("utf-8")
-            # 获取包含需要结果页面的DIV标签
-            # 获取包含需要结果页面的DIV标签
-            response_DIV = re.findall('<li class="chief-recom-item">(.*?)</li>', response)
-            # 获取所需要的所有A标签
-            response_A = re.findall('a href="(.*?)"', str(response_DIV))
-            # 获取所有需要的DIV标签
-            response_IMG = re.findall('img src="(.*?)@', str(response_DIV))
-            for i in range(len(response_A)):
-                if i < 6:
-                    print("获得URL：%s"%(response_A[i]))
+    def All_Bilibili_Img_Url(self):
+        All_Url_List = [
+            "https://www.bilibili.com/anime/",
+            "https://www.bilibili.com/anime/",
+            "https://www.bilibili.com/tv/",
+            "https://www.bilibili.com/movie/",
+            "https://www.bilibili.com/documentary/"
+
+        ]
+        return All_Url_List
+
+    def Get_BiliBili_Img(self,golbalData):
+        All_Url_List = self.All_Bilibili_Img_Url()
+        for bilibiliUrl in All_Url_List:
+            try:
+                r = requests.get(bilibiliUrl, headers=self.get_request_headers(), timeout=5)
+                # 获取网页所有HTML标签
+                response = r.content.decode("utf-8")
+                # 获取包含需要结果页面的DIV标签
+                response_DIV = re.findall('<div class="carou-images">(.*?)</div>', response)
+                # 获取所需要的所有A标签
+                response_A = re.findall('a href="(.*?)"', str(response_DIV))
+                # 获取所有需要的DIV标签
+                response_IMG = re.findall('img src="(.*?)@', str(response_DIV))
+                for i in range(len(response_A)):
+                    print("获得URL：%s" % (response_A[i]))
                     golbalData["BiliBili"][response_A[i]] = response_IMG[i]
-        except BaseException as error:
-            print("Get_BiliBili_Img_Url_guochan----------\n", error)
+            except BaseException as error:
+                print("Get_BiliBili_Img_Url----------\n", error)
 
     def get_request_headers(self):
         # 用户代理User-Agent列表
--- SpaceStation2/test.py
+++ SpaceStation2/test.py
@@ -1,37 +1,36 @@
-import re
 import requests
-import random
+import re
 
 
-def get_request_headers():
-    # 用户代理User-Agent列表
-    USER_AGENTS = [
-        "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36",
-        "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:69.0) Gecko/20100101 Firefox/69.0"
+def All_Bilibili_Img_Url():
+    All_Url_List = [
+        "https://www.bilibili.com/anime/",
+        "https://www.bilibili.com/anime/",
+        "https://www.bilibili.com/tv/",
+        "https://www.bilibili.com/movie/",
+        "https://www.bilibili.com/documentary/"
+
     ]
-
-    headers = {
-        "User-Agent": random.choice(USER_AGENTS),
-    }
-    return headers
-
-def Get_BiliBili_Img_Url_guochan():
-    try:
-        r = requests.get("https://www.bilibili.com/guochuang",headers=get_request_headers(),timeout=5)
-        # 获取网页所有HTML标签
-        response = r.content.decode("utf-8")
-        # 获取包含需要结果页面的DIV标签
-        response_DIV = re.findall('<li class="chief-recom-item">(.*?)</li>', response)
-        # 获取所需要的所有A标签
-        response_A = re.findall('a href="(.*?)"', str(response_DIV))
-        # 获取所有需要的DIV标签
-        response_IMG = re.findall('img src="(.*?)@', str(response_DIV))
-        for i in range(len(response_A)):
-            if i < 6:
-                golbalData["BiliBili"][response_A[i]] = response_IMG[i]
-    except BaseException as error:
-        print("Get_BiliBili_Img_Url_guochan----------\n", error)
+    return All_Url_List
 
 
+def Get_BiliBili_Img_Url():
+    All_Url_List = All_Bilibili_Img_Url()
+    for bilibiliUrl in All_Url_List:
+        try:
+            r = requests.get(bilibiliUrl, timeout=5)
+            # 获取网页所有HTML标签
+            response = r.content.decode("utf-8")
+            # 获取包含需要结果页面的DIV标签
+            response_DIV = re.findall('<div class="carou-images">(.*?)</div>', response)
+            # 获取所需要的所有A标签
+            response_A = re.findall('a href="(.*?)"', str(response_DIV))
+            # 获取所有需要的DIV标签
+            response_IMG = re.findall('img src="(.*?)@', str(response_DIV))
+            for i in range(len(response_A)):
+                print("获得URL：%s" % (response_A[i]))
+        except:
+            print()
 
-Get_BiliBili_Img_Url_guochan()
+
+Get_BiliBili_Img_Url()
